cd4b698a8978cfddbae4f67a4cf1456ee9c20e6e
Sandeep Kumar <sk5545708@gmail.com>
2021-05-16 00:02:39 +0530

    added some features

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var a;
 a = document.getElementById("hide_show");
 
+var b = document.getElementById("foo1");
+var c = document.getElementById("foo2");
+
 function hide() {
+
     if (a.style.display === "none") {
         document.getElementById("hide_show").style.display = "block";
+        document.getElementById("foo1").style.display = "none";
+        document.getElementById("foo2").style.display = "block";
         return 0;
     } else {
         document.getElementById("hide_show").style.display = "none";
+        document.getElementById("foo1").style.display = "block";
+        document.getElementById("foo2").style.display = "none";
         return 1;
     }
 }
